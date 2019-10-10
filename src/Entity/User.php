5d5178e8df66db1841e9f4720c383966183eb8c6
Artemis-Haven <artemis.hacktivation@gmail.com>
2019-10-10 21:32:00 +0000

    Added FOSUserBundle and EasyAdminBundle

---
+++ src/Entity/User.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}

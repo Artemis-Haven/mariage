062c43d9e8edb554829b1eb306d5f371b72483ac
Artemis-Haven <artemis.hacktivation@gmail.com>
2019-10-17 20:51:44 +0000

    Send an email to notify about the activation of an account

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,14 +5,18 @@ namespace App\Controller;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\EasyAdminController;
 use FOS\UserBundle\Model\UserManagerInterface;
 use App\Entity\Gift;
+use App\Entity\User;
 
 class AdminController extends EasyAdminController
 {
     private $userManager;
+
+    private $mailer;
     
-    public function __construct(UserManagerInterface $userManager)
+    public function __construct(UserManagerInterface $userManager, \Swift_Mailer $mailer)
     {
         $this->userManager = $userManager;
+        $this->mailer = $mailer;
     }
 
     public function createNewUserEntity()
@@ -38,5 +42,35 @@ class AdminController extends EasyAdminController
     {
         $gift->setCreatedAt(new \DateTime('now'));
         parent::persistEntity($gift);
+    }
+
+    
+    public function emailWhenActivatedAction()
+    {
+        // change the properties of the given entity and save the changes
+        $id = $this->request->query->get('id');
+        $user = $this->em->getRepository(User::class)->find($id);
+        
+        $email = (new \Swift_Message("Activation de votre compte sur mariage-manon-remi.fr !"))
+            ->setFrom($this->getParameter('mail_from'))
+            ->setTo($user->getEmail())
+            ->setBody(
+                $this->renderView(
+                    'emails/activation.html.twig', 
+                    [
+                        'user' => $user
+                    ]
+                ),
+                'text/html'
+            );
+        
+        $this->mailer->send($email);
+        
+        $this->addFlash('success', "Un e-mail de notification d'activation a été envoyé à ".$user->getEmail());
+
+        return $this->redirectToRoute('easyadmin', array(
+            'action' => 'list',
+            'entity' => $this->request->query->get('entity'),
+        ));
     }
 }
